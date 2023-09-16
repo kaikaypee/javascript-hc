709c1fe85f153fb7eb2f330259e5c64d716be0cb
kaikaypee <144536053+kaikaypee@users.noreply.github.com>
2023-09-16 00:45:36 +0000

    datatypes basics done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all JS Code as newer version
+
+// alert ( 3 + 3) //we are using Node.js, not browser
+
+// console.log( 3 
+//     +
+//     3) // code readability should be high
+
+//   console.log("KAY")  
+
+let name = "KAY"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// bigint =>
+// string =>""
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined =>  
+// symbol => uniqueness
+
+//object =>
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
